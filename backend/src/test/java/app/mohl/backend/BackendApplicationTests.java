3b2466002fb1eefb4e4429166e69c2c12ad9f967
Matthew Grayson <matthew.grayson@associates.cisa.dhs.gov>
2024-02-24 16:06:50 -0600

    Add backend skeleton.

---
+++ backend/src/test/java/app/mohl/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.mohl.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
