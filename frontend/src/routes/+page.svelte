e3e11176b450979b4ce5f05e71aaeea07aeaa8e4
Matthew Grayson <matthew.grayson@associates.cisa.dhs.gov>
2024-02-24 22:07:59 -0600

    Wireframing; add base emojis.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="wrapper">
+    <div id="header">
+        <h1>Header</h1>
+    </div>
+    <div id="canvas">
+        <h2>Canvas</h2>
+        <p>Content goes here</p>
+    </div>
+    <div id="sidebar">
+        <h3>Sidebar</h3>
+        <p>Sidebar goes here</p>
+    </div>
+</div>
+
+<style>
+    #wrapper {
+        background-color: #111;
+        display: grid;
+        grid-template-columns: 4fr 1fr;
+        grid-template-rows: 100px 1fr;
+        grid-template-areas: "header header" "canvas sidebar";
+        height: 100vh;
+    }
+
+    #header {
+        color: #eee;
+        grid-area: header;
+    }
+
+    #canvas {
+        color: #eee;
+        grid-area: canvas;
+        background-color: #111;
+        border: black 1px solid;
+        height: 100%;
+    }
+
+    #sidebar {
+        color: #eee;
+        grid-area: sidebar;
+        background-color: #111;
+        height: 100%;
+    }
+</style>
